782adbe0453584eff89eaa227c948afb502921e2
Shao Yang <hongsy2006@gmail.com>
2019-08-23 19:08:40 +0800

    Solve 9.12

--- epi_judge_python/tree_from_preorder_inorder.py
+++ epi_judge_python/tree_from_preorder_inorder.py
@@ -1,10 +1,19 @@
+from binary_tree_node import BinaryTreeNode 
 from test_framework import generic_test
 
 
 def binary_tree_from_preorder_inorder(preorder, inorder):
-    # TODO - you fill in here.
-    return None
+    if not inorder:
+        return None
 
+    tree = BinaryTreeNode(preorder[0])
+    x = inorder.index(preorder[0])
+    left, right = inorder[:x], inorder[x+1:]
+    if left:
+        tree.left = binary_tree_from_preorder_inorder(preorder[1:len(left)+1], left)
+    if right:
+        tree.right = binary_tree_from_preorder_inorder(preorder[len(left)+1:], right)
+    return tree
 
 if __name__ == '__main__':
     exit(
