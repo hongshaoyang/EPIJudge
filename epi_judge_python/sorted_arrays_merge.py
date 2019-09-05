bf4caa01568bbc92030b417b6ae9fc4920fee31e
Shao Yang <hongsy2006@gmail.com>
2019-09-05 11:11:39 +0800

    Solve 10.1

--- epi_judge_python/sorted_arrays_merge.py
+++ epi_judge_python/sorted_arrays_merge.py
@@ -1,9 +1,26 @@
+import heapq
 from test_framework import generic_test
 
 
 def merge_sorted_arrays(sorted_arrays):
-    # TODO - you fill in here.
-    return []
+    # push zeroth elements of all arrs to minheap
+    min_heap = [(arr.pop(0), i) for i, arr in enumerate(sorted_arrays) if arr]
+    heapq.heapify(min_heap)
+
+    res = []
+    while any(sorted_arrays):
+        # pop the minheap and add the next element from that arr to the list
+        num, idx = heapq.heappop(min_heap)
+        res.append(num)
+        if sorted_arrays[idx]:
+            heapq.heappush(min_heap, (sorted_arrays[idx].pop(0), idx))
+    
+    # continue popping the minheap until it's done
+    while min_heap:
+        res.append(heapq.heappop(min_heap)[0])
+    return res
+
+
 
 
 if __name__ == '__main__':
