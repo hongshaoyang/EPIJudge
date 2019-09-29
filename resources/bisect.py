e6d08d425566c0400c402c4b9d3d9638a1e35867
Shao Yang <hongsy2006@gmail.com>
2019-09-29 14:04:23 +0800

    Fix bisect resource

--- resources/bisect.py
+++ resources/bisect.py
@@ -1,5 +1,4 @@
 import bisect
-
 from collections import namedtuple
 
 # Given an array of students, sorted by descending GPA, with ties broken on name.
@@ -13,8 +12,7 @@ def comp_gpa(student: Student):
 def search_student(students, target, comp_gpa):
     # custom comparator
     i = bisect.bisect_left([comp_gpa(s) for s in students], comp_gpa(target))
-    return 0 <= i < len(studnets) and students[i] == target
-
+    return 0 <= i < len(students) and students[i] == target
 
 bisect.bisect_left(a, x) # find 1st ele not less than target
 bisect.bisect_right(a, x) # find 1st ele greater than target
