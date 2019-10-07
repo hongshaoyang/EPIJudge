392e3cb83c6c52626b1ba9ba4b5b4971745cd659
Shao Yang <hongsy2006@gmail.com>
2019-10-07 16:43:23 +0800

    Solve 12.2

--- epi_judge_python/12-2_is_anonymous_letter_constructible.py
+++ epi_judge_python/12-2_is_anonymous_letter_constructible.py
@@ -1,9 +1,19 @@
+from collections import defaultdict
 from test_framework import generic_test
 
 
 def is_letter_constructible_from_magazine(letter_text, magazine_text):
-    # TODO - you fill in here.
-    return True
+    # compute freqs
+    count = defaultdict(int)
+    for char in magazine_text:
+        count[char] += 1
+    for char in letter_text:
+        count[char] -= 1
+    # if any of the freqs are -ve, the letter cannot be written
+    cannot_be_written = any(v < 0 for v in count.values())
+    return not cannot_be_written
+
+
 
 
 if __name__ == '__main__':
